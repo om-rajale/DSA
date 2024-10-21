281aaeb0d43e307deca64cdd8b60c64227ad1ce7
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2024-10-22 02:22:14 +0530

    Add files via upload

---
+++ singly linkedlist.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+        
+    def print_LL(self):
+        if self.head == None:
+            print('linkedlist is empty')
+        else:
+            n = self.head
+            print('linkedlist is not empty')
+            while n is not None:
+                print(n.data)
+                n = n.ref
+
+LL1 = Linkedlist()
+LL1.print_LL()
