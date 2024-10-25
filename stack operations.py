a98a3d05bbf8a1c10de4c8245580bde53e96535d
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2024-10-25 23:25:52 +0530

    Add files via upload

---
+++ stack operations.py
@@ -0,0 +1,24 @@
+stack=[]
+def push():
+    elements=input("add elements:")
+    stack.append(elements)
+    print(stack)
+def pop():
+    if not stack:
+        print("stack is empty")
+    else:
+        d = stack.pop()
+        print("remove elements:",d)
+        print(stack)
+while True:
+    choice = int(input("enter choice 1.push,2.pop.3.quit"))
+    if choice==1:
+        push()
+    elif choice==2:
+        pop()
+    elif choice==3:
+        break
+    else:
+        print("enter correct choice")
+        
+    
